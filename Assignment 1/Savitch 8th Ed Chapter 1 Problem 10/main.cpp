5a76155be3b0688ddddc1d3a20cf1572df4012bc
alveykami <kadverb@yahoo.com>
2016-01-05 12:05:47 -0800

    Signed-off-by: alveykami <kadverb@yahoo.com>

---
+++ Assignment 1/Savitch 8th Ed Chapter 1 Problem 10/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Kami Alvey
+ * Created on January 5, 2016, 11:43 AM
+ * Purpose: Savitch 8th Edition Chapter 1 Problem 10
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter='X';
+    //Input Data
+    
+    //Calculate or Map Inputs
+    
+    //Output Results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+   
+    
+    //Exit Stage Right
+    return 0;
+}
+
